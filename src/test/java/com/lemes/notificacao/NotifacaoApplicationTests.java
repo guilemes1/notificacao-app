baa67fe99307342fb7c4f42668c4ae2ab2d01c51
Guilherme Lemes <guilhermelemes08@gmail.com>
2024-10-13 14:19:08 -0300

    Primeiro commit - Microsserviço de notificação

---
+++ src/test/java/com/lemes/notificacao/NotifacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lemes.notificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
